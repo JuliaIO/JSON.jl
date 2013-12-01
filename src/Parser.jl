a015ab1c2056ae174d960f58e146be7d8331d531
Jiahao Chen <jiahao@mit.edu>
2013-12-01 16:29:39 -0500

    Formatting cleanup

--- src/Parser.jl
+++ src/Parser.jl
@@ -76,7 +76,7 @@ print_trace(trace::Trace) = println(
 
 function _search(haystack::String, needle::Union(String, Regex, Char), _start::Int)
     range = search(haystack, needle, _start)
-    return (first(range), last(range))
+    first(range), last(range)
 end
 
 # Eat up spaces starting at s.
@@ -106,10 +106,8 @@ function _error(message::String, str::String, s::Int, e::Int)
     lines = _count_before(str, '\n', s)
     # Replace all special multi-line/multi-space characters with a space.
     strnl = replace(str, r"[\b\f\n\r\t\s]", " ")
-    # Left index
-    li = (s > 20) ? s - 9 : 1
-    # Right index
-    ri = min(e, s + 20)
+    li = (s > 20) ? s - 9 : 1 # Left index
+    ri = min(e, s + 20)       # Right index
     error(
       message *
       "\nLine: " * string(lines) *
@@ -125,64 +123,44 @@ function parse_array(str::String, s::Int, e::Int, tracer::Tracer)
     s += 1 # Skip over the '['
     _array = TYPES[]
     s = chomp_space(str, s, e)
-    # Check for empty array
-    if str[s] == ']'
-      trace_out(tracer, _ti)
-      return (_array, s + 1, e)
-    end
-    # Extract values from array
-    while true
-        # Extract value
-        v, s, e = parse_value(str, s, e, tracer)
+    str[s]==']' && return _array, s+1, e # Check for empty array
+    while true # Extract values from array
+        v, s, e = parse_value(str, s, e) # Extract value
         push!(_array, v)
         # Eat up trailing whitespace
         s = chomp_space(str, s, e)
         c = str[s]
         if c == ','
-          s += 1
-          continue
+            s += 1
+            continue
         elseif c == ']'
-          s += 1
-          break
+            s += 1
+            break
         else
-          _error(
-            "Unexpected char: " * string(c),
-            str, s, e
-          )
+            _error("Unexpected char: " * string(c), str, s, e)
         end
     end
     trace_out(tracer, _ti)
-    return (_array, s, e)
+    return _array, s, e
 end
 
 function parse_object(str::String, s::Int, e::Int, tracer::Tracer)
     _ti = trace_in(tracer, "object")
     s += 1 # Skip over opening '{'
     obj = Dict{KEY_TYPES,TYPES}()
-    # Eat up some space
     s = chomp_space(str, s, e)
-    # Check for empty object
-    if str[s] == '}'
-        trace_out(tracer, _ti)
-        return (obj, s + 1, e)
-    end
+    str[s]=='}' && return obj, s+1, e # Check for empty object
     while true
         s = chomp_space(str, s, e)
-        # Key
-        _key, s, e = parse_string(str, s, e, tracer)
-        # Separator
-        ss, se = _search(str, ':', s)
+        _key, s, e = parse_string(str, s, e)  # Key
+        ss, se = _search(str, ':', s)         # Separator
         # TODO: Error handling if it doesn't find the separator
         ss < 1 && _error( "Separator not found ", str, s, e)
-        # Skip over separator
-        s = se + 1
-        # Value
-        _value, s, e = parse_value(str, s, e, tracer)
-        obj[_key] = _value # Building object
-        
+        s = se + 1                            # Skip over separator
+        _value, s, e = parse_value(str, s, e) # Value
+        obj[_key] = _value                    # Building object
         s = chomp_space(str, s, e)
-        # Find the next pair or end of object
-        c = str[s]
+        c = str[s] # Find the next pair or end of object
         if c == ','
             s += 1
             continue
@@ -194,68 +172,44 @@ function parse_object(str::String, s::Int, e::Int, tracer::Tracer)
         end
     end
     trace_out(tracer, _ti)
-    return (obj, s, e)
+    return obj, s, e
 end
 
-# Special characters for parse_string
-const _dq = uint8('"' )
-const _bs = uint8('\\')
-const _fs = uint8('/' )
-const _b  = uint8('\b')
-const _f  = uint8('\f')
-const _n  = uint8('\n')
-const _r  = uint8('\r')
-const _t  = uint8('\t')
 # TODO: Try to find ways to improve the performance of this (currently one
 #       of the slowest parsing methods).
 function parse_string(str::String, s::Int, e::Int, tracer::Tracer)
     _ti = trace_in(tracer, "string")
     str[s]=='"' || _error("Missing opening string char", str, s, e)
     s = nextind(str, s) # Skip over opening '"'
-    
     b = IOBuffer()
-    
     found_end = false
     while s <= e
         c = str[s]
         if c == '\\'
             s = nextind(str, s)
             c = str[s]
-            if c == 'u'
-                # Unicode escape
-                # Get the string
-                u = unescape_string(str[s - 1:s + 4])
-                # Get the uint8s for the string
-                d = bytestring(u).data
-                # append!(o, d)
+            if c == 'u' # Unicode escape
+                u = unescape_string(str[s - 1:s + 4]) # Get the string
+                d = bytestring(u).data # Get the uint8s for the string
                 write(b, bytestring(u))
                 # Skip over those next four characters
                 [s = nextind(str, s) for _ = 1:4]
-            elseif c == '"'
-                write(b, '"')
-            elseif c == '\\'
-                write(b, '\\')
-            elseif c == '/'
-                write(b, '/')
-            elseif c == 'b'
-                write(b, '\b')
-            elseif c == 'f'
-                write(b, '\f')
-            elseif c == 'n'
-                write(b, '\n')
-            elseif c == 'r'
-                write(b, '\r')
-            elseif c == 't'
-                write(b, '\t')
-            else
-                _error("Unrecognized escaped character: " * string(c), str, s, e)
+            elseif c == '"'  write(b, '"' )
+            elseif c == '\\' write(b, '\\')
+            elseif c == '/'  write(b, '/' )
+            elseif c == 'b'  write(b, '\b')
+            elseif c == 'f'  write(b, '\f')
+            elseif c == 'n'  write(b, '\n')
+            elseif c == 'r'  write(b, '\r')
+            elseif c == 't'  write(b, '\t')
+            else _error("Unrecognized escaped character: " * string(c), str, s, e)
             end
         elseif c == '"'
-          found_end = true
-          s = nextind(str, s)
-          break
+            found_end = true
+            s = nextind(str, s)
+            break
         else
-          write(b, c)
+            write(b, c)
         end
         s = nextind(str, s)
     end
@@ -269,31 +223,26 @@ end
 function parse_simple(str::String, s::Int, e::Int, tracer::Tracer)
     _ti = trace_in(tracer, "simple")
     c = str[s]
-    if c == 't' && str[s + 3] == 'e'
-        # Looks like "true"
+    if c == 't' && str[s + 3] == 'e'     # Looks like "true"
         ret = (true, s + 4, e)
-    elseif c == 'f' && str[s + 4] == 'e'
-        # Looks like "false"
+    elseif c == 'f' && str[s + 4] == 'e' # Looks like "false"
         ret = (false, s + 5, e)
-    elseif c == 'n' && str[s + 3] == 'l'
-        # Looks like "null"
+    elseif c == 'n' && str[s + 3] == 'l' # Looks like "null"
         ret = (nothing, s + 4, e)
     else
         _error("Unknown simple: " * string(c), str, s, e)
     end
     trace_out(tracer, _ti)
-    return ret
+    ret
 end
 
 function parse_value(str::String, s::Int, e::Int, tracer::Tracer)
-    #_ti = trace_in(tracer, "value")
+    _ti = trace_in(tracer, "value")
     s = chomp_space(str, s, e)
-    # Nothing left
-    s == e && return nothing, s, e
+    s==e && return nothing, s, e # Nothing left
     
     ch = str[s]
-    if ch == '"'
-        ret = parse_string(str, s, e, tracer)
+    if ch == '"' ret = parse_string(str, s, e, tracer)
     elseif ch == '{'
         ret = parse_object(str, s, e, tracer)
     elseif (ch >= '0' && ch <= '9') || ch == '-'
@@ -305,7 +254,7 @@ function parse_value(str::String, s::Int, e::Int, tracer::Tracer)
     else
         _error("Unknown value", str, s, e)
     end
-    #trace_out(tracer, _ti)
+    trace_out(tracer, _ti)
     return ret
 end
 
@@ -313,8 +262,8 @@ function parse_number(str::String, s::Int, e::Int, tracer::Tracer)
     _ti = trace_in(tracer, "number")
     
     p = s
-    # Look for negative
-    if str[p] == '-'
+    # Look for sign
+    if str[p]=='-' 
         p += 1
     end
     # Look for number
@@ -327,54 +276,41 @@ function parse_number(str::String, s::Int, e::Int, tracer::Tracer)
             is_float = false
         end
     elseif str[p] > '0' && str[p] <= '9'
-      p += 1
-      # Match more digits
-      while str[p] >= '0' && str[p] <= '9'
-          p += 1
-      end
-      if str[p] == '.'
-          p += 1
-          is_float = true
-      else
-          is_float = false
-      end
-    else
-      _error(
-        "Unrecognized number",
-        str, p, e
-      )
-    end
-    if is_float
-      # Match digits after decimal
-      while str[p] >= '0' && str[p] <= '9'
         p += 1
-      end
+        # Match more digits
+        while str[p] >= '0' && str[p] <= '9'
+            p += 1
+        end
+        if str[p] == '.'
+            p += 1
+            is_float = true
+        else
+            is_float = false
+        end
     else
-      # Not decimal
+        _error("Unrecognized number", str, p, e)
+    end
+    if is_float # Match digits after decimal
+        while str[p] >= '0' && str[p] <= '9'
+            p += 1
+        end
     end
     if str[p] == 'E' || str[p] == 'e' || str[p] == 'f'
-      p += 1
-      is_float = true
-      # Exponent sign
-      if str[p] == '-' || str[p] == '+'
         p += 1
-      end
-      # Exponent digits
-      while str[p] >= '0' && str[p] <= '9'
-        p += 1
-      end
+        is_float = true
+        if str[p] == '-' || str[p] == '+' # Exponent sign
+            p += 1
+        end
+        while str[p] >= '0' && str[p] <= '9' # Exponent digits
+            p += 1
+        end
     end
     
     vs = str[s:p - 1]
-    if is_float
-      v = parsefloat(vs)
-    else
-      v = parseint(vs)
-    end
+    v = (is_float ? parsefloat : parseint)(vs)
     
     trace_out(tracer, _ti, vs)
-    s = p
-    return (v, s, e)
+    return v, p, e
 end
 
 function parse(str::String, trace::Bool=false)
