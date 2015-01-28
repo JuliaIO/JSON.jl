JuliaIO/JSON.jl

commit 9b416497668ebf88be0f8311606c6e63973990f3

Simplify ordered dictionary loading code
`try/catch` no longer returns `false` when no `catch` block is defined and an exception is thrown. Ref: JuliaLang/julia#9320

src/Parser.jl
@@ -2,13 +2,10 @@ module Parser #JSON
 
 using Compat
 
-_HAVE_ORDERED_DICT = try
-    import DataStructures
-    true
-end
-if _HAVE_ORDERED_DICT
+#Define ordered dictionary from DataStructures if present
+try
     import DataStructures.OrderedDict
-else
+catch
     function OrderedDict(key_types, types)
         Base.warn_once("Ordered JSON object parsing is not available.\nRun `Pkg.add(\"DataStructures.jl\")` to enable.")
         Dict{key_types, types}()

commit 0dc8492080bb0c8001af350dbd3b289a3e00d4d8

Fix #91
* It turns out that 9b416497 broke things when no one was looking.
* This reverts OrderedDict loading to a more verbose but functional
  state, and adds a test.

src/Parser.jl
@@ -3,9 +3,16 @@ module Parser #JSON
 using Compat
 
 #Define ordered dictionary from DataStructures if present
-try
-    import DataStructures.OrderedDict
+_HAVE_DATASTRUCTURES = try
+    using DataStructures
+    true
 catch
+    false
+end
+
+if _HAVE_DATASTRUCTURES
+    import DataStructures.OrderedDict
+else
     function OrderedDict(key_types, types)
         Base.warn_once("Ordered JSON object parsing is not available.\nRun `Pkg.add(\"DataStructures.jl\")` to enable.")
         Dict{key_types, types}()

test/REQUIRE
@@ -1,0 +1,1 @@
+DataStructures

test/runtests.jl
@@ -1,14 +1,11 @@
 using JSON
 using Base.Test
-using Compat 
+using Compat
+import DataStructures
 
 include(joinpath(dirname(@__FILE__),"json_samples.jl"))
 
-# compatibility for julia version 0.2
-if !isdefined(:put!)
-        const put! = put
-        const take! = take
-end
+@test JSON.parse("{\"x\": 3}"; ordered = true) == DataStructures.OrderedDict{String,Any}(["x"],[3])
 
 # Test definitions -------
 validate_c(c) = begin