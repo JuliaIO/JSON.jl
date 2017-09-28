b4a4e338029bb999589dfabc6d90f5ae2fdd08d8
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-09-27 22:29:41 -0400

    Restore 100% test coverage (#218)

--- src/Writer.jl
+++ src/Writer.jl
@@ -250,13 +250,6 @@ function show_json(io::SC, s::CS, x::IsPrintedAsString)
 end
 
 function show_json(io::SC, s::CS, x::Union{Integer, AbstractFloat})
-    # workaround for issue in Julia 0.5.x where Float32 values are printed as
-    # 3.4f-5 instead of 3.4e-5
-    @static if VERSION < v"0.6.0-dev.788"
-        if isa(x, Float32)
-            return show_json(io, s, Float64(x))
-        end
-    end
     if isfinite(x)
         Base.print(io, x)
     else
