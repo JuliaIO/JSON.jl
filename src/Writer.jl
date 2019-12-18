71767f1274002f78855a45882a82f93360008853
0x005c <52650857+0x005c@users.noreply.github.com>
2019-12-18 16:47:00 +0900

    Fix show_json to use Float64 instead of AbstractFloat

--- src/Writer.jl
+++ src/Writer.jl
@@ -261,14 +261,7 @@ end
 function show_json(io::SC, s::CS, f::AbstractFloat)
     if isfinite(f)
         str = string(f)
-        # use 'f' for exponent is not allowed in JSON
-        for i in 1:lastindex(str)
-            if str[i] == 'f'
-                Base.print(io, join([str[1:i-1], "e", str[i+1:lastindex(str)]]))
-                return
-            end
-        end
-        Base.print(io, str)
+        Base.print(io, convert(Float64, f))
     else
         show_null(io)
     end
