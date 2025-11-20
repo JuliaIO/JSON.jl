ad975e51b4383e5660af46f691115fce187466fe
Horikawa Yuto <hyrodium@gmail.com>
2025-11-21 01:44:26 +0900

    sort sections

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,15 @@
 using Documenter, JSON
 
-makedocs(modules = [JSON], sitename = "JSON.jl")
+makedocs(
+    modules = [JSON],
+    sitename = "JSON.jl",
+    pages = [
+        "Home" => "index.md",
+        "JSON Writing" => "writing.md",
+        "JSON Reading" => "reading.md",
+        "Migration Guides" => "migrate.md",
+        "API Reference" => "reference.md",
+    ],
+)
 
 deploydocs(repo = "github.com/JuliaIO/JSON.jl.git", push_preview = true)
